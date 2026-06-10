fa71319dbee9595f881cca028a4e4b4235732ac7
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2026-06-10 23:48:38 +0100

    sorted envs

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -2,14 +2,6 @@
  * Configurations and settings for the application.
  */
 export const settings = {
-    // TODO: ADD ENVS TO .env
-    "server": {
-        VITE_ENV: String(import.meta.env.VITE_ENV),
-        VITE_APP_PORT: Number(import.meta.env.VITE_APP_PORT),
-        VITE_APP_BASE_URL: String(import.meta.env.VITE_APP_BASE_URL),
-        VITE_APP_TITLE: String(import.meta.env.VITE_APP_TITLE)
-    },
-
     "api": {
         BASE_URL: import.meta.env.VITE_DOCU_CHAT_API_BASE_URL,
         BASE_VERSION: import.meta.env.VITE_DOCU_CHAT_API_VERSION ?? "1",
@@ -17,6 +9,6 @@ export const settings = {
     },
 
     "documents": {
-        MAX_MB_PER_CHAT: Number(import.meta.env.VITE_MAX_MB_PER_CHAT) || 10,
+        MAX_MB_PER_CHAT: Number(import.meta.env.VITE_MAX_MB_PER_CHAT) || 1,
     }
 }
