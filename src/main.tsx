ba2e9810a54316e2effd701ee1d960bdd7f5386a
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2026-06-23 02:38:47 +0100

    feat: added react router to allow for the switching of pages - linked the privay and terms pages in main.tsx - Updated the footer.json to include the links to privacy and terms page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import { PrivacyPage } from "./pages/PrivacyPage";
+import { TermsPage } from "./pages/TermsPage";
 import "./styles/index.css";
 import * as Sentry from "@sentry/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<Sentry.ErrorBoundary fallback={<p>Something went wrong.</p>}>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/privacy" element={<PrivacyPage />} />
+				<Route path="/terms" element={<TermsPage />} />
+			</Routes>
+		</BrowserRouter>
 	</Sentry.ErrorBoundary>,
 );
