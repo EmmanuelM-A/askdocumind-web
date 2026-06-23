118994ef092f1f6c03ec06af547b69b7936b0375
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2026-06-23 01:11:17 +0100

    feat: setup sentry and its configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<Sentry.ErrorBoundary fallback={<p>Something went wrong.</p>}>
+		<App />
+	</Sentry.ErrorBoundary>,
+);
